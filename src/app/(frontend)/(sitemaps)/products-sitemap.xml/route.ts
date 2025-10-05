324437680fadfcec16eec5c35b7b53247b938a91
codenikin <codenikin@gmail.com>
2025-10-05 14:08:30 +0530

    new

--- src/app/(frontend)/(sitemaps)/products-sitemap.xml/route.ts
+++ src/app/(frontend)/(sitemaps)/products-sitemap.xml/route.ts
@@ -4,102 +4,101 @@ import config from '@payload-config'
 import { unstable_cache } from 'next/cache'
 import { Product, Subcategory } from '@/payload-types'
 
-const getPagesSitemap = unstable_cache(
-  async () => {
-    const payload = await getPayload({ config })
-    const SITE_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'https://raystrading.com/'
+const getPagesSitemap = async () => {
+  const payload = await getPayload({ config })
+  const SITE_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'https://raystrading.com/'
 
-    const results = await payload.find({
-      collection: 'categories',
-      overrideAccess: false,
-      pagination: false,
-      depth: 3,
-    })
+  const results = await payload.find({
+    collection: 'categories',
+    overrideAccess: false,
+    pagination: false,
+    depth: 4,
+  })
 
-    const dateFallback = new Date().toISOString()
-    const categorySitemap = results.docs
-      ? results.docs
-          .filter((category) => Boolean(category?.slug))
-          .map((category) => {
+  const dateFallback = new Date().toISOString()
+  const categorySitemap = results.docs
+    ? results.docs
+        .filter((category) => Boolean(category?.slug))
+        .map((category) => {
+          return {
+            loc: `${SITE_URL}/products/${category?.slug}`,
+            lastmod: category.updatedAt || dateFallback,
+          }
+        })
+    : []
+
+  const subCategorySitemap = results.docs
+    ? results.docs
+        .filter((category) => Boolean(category?.slug))
+        .map((category) => {
+          return category.subcategories?.docs?.map((subCategory) => {
+            const subCat = subCategory as Subcategory
             return {
-              loc: `${SITE_URL}/products/${category?.slug}`,
+              loc: `${SITE_URL}/products/${category?.slug}/${subCat.slug}`,
               lastmod: category.updatedAt || dateFallback,
             }
           })
-      : []
+        })
+        .filter((item) => item !== undefined)
+        .flat()
+    : []
+  const productSitemap = results.docs
+    ? (
+        await Promise.all(
+          results.docs
+            .filter((category) => Boolean(category?.slug))
+            .map(async (category) => {
+              return await Promise.all(
+                category.subcategories?.docs?.map(async (subCategory) => {
+                  const subCat = subCategory as Subcategory
+                  return await Promise.all(
+                    subCat.products?.docs?.map(async (product) => {
+                      let prod = product as Product | number
 
-    const subCategorySitemap = results.docs
-      ? results.docs
-          .filter((category) => Boolean(category?.slug))
-          .map((category) => {
-            return category.subcategories?.docs?.map((subCategory) => {
-              const subCat = subCategory as Subcategory
-              return {
-                loc: `${SITE_URL}/products/${category?.slug}/${subCat.slug}`,
-                lastmod: category.updatedAt || dateFallback,
-              }
-            })
-          })
-          .filter((item) => item !== undefined)
-          .flat()
-      : []
-
-    const productSitemap = results.docs
-      ? (
-          await Promise.all(
-            results.docs
-              .filter((category) => Boolean(category?.slug))
-              .map(async (category) => {
-                return await Promise.all(
-                  category.subcategories?.docs?.map(async (subCategory) => {
-                    const subCat = subCategory as Subcategory
-                    return await Promise.all(
-                      subCat.products?.docs?.map(async (product) => {
-                        let prod = product as Product | number
-                        if (typeof prod === 'number') {
-                          prod = await payload.findByID({ collection: 'products', id: prod })
-                        }
-                        if (!prod || typeof prod !== 'object' || !('slug' in prod)) return undefined
-                        return {
-                          loc: `${SITE_URL}/products/${category?.slug}/${subCat.slug}/${prod.slug}`,
-                          lastmod: prod.updatedAt || dateFallback,
-                        }
-                      }) || [],
-                    )
-                  }) || [],
-                )
-              }),
-          )
+                      if (typeof prod === 'number') {
+                        prod = await payload.findByID({ collection: 'products', id: prod })
+                      }
+                      // if (!prod || typeof prod !== 'object' || !('slug' in prod)) return undefined
+                      return {
+                        loc: `${SITE_URL}/products/${category?.slug}/${subCat.slug}/${prod?.slug}`,
+                        lastmod: prod.updatedAt || dateFallback,
+                      }
+                    }) || [],
+                  )
+                }) || [],
+              )
+            }),
         )
-          .flat(2)
-          .filter((item) => item !== undefined)
-      : []
+      )
+        .flat(2)
+        .filter((item) => item !== undefined)
+    : []
 
-    const sitemapArray = [...categorySitemap, ...subCategorySitemap, ...productSitemap]
-      .filter(Boolean)
-      .map((item) => {
-        let lastmod = item.lastmod
-        if (
-          lastmod &&
-          typeof lastmod === 'object' &&
-          Object.prototype.toString.call(lastmod) === '[object Date]'
-        ) {
-          lastmod = (lastmod as Date).toISOString()
-        } else if (typeof lastmod !== 'string') {
-          lastmod = dateFallback
-        }
-        return {
-          ...item,
-          lastmod,
-        }
-      })
-    return sitemapArray
-  },
-  ['pages-sitemap'],
-  {
-    tags: ['pages-sitemap'],
-  },
-)
+  const sitemapArray = [...categorySitemap, ...subCategorySitemap, ...productSitemap]
+    .filter(Boolean)
+    .map((item) => {
+      let lastmod = item.lastmod
+      if (
+        lastmod &&
+        typeof lastmod === 'object' &&
+        Object.prototype.toString.call(lastmod) === '[object Date]'
+      ) {
+        lastmod = (lastmod as Date).toISOString()
+      } else if (typeof lastmod !== 'string') {
+        lastmod = dateFallback
+      }
+      return {
+        ...item,
+        lastmod,
+      }
+    })
+  return sitemapArray
+}
+//   ['pages-sitemap'],
+//   {
+//     tags: ['pages-sitemap'],
+//   },
+// )
 
 export async function GET() {
   const sitemap = await getPagesSitemap()
